88874f3d7ecbd22e1eb8ec51150e4c0f484fe90a
LOckETHQC <90512716+LOck-ethqc@users.noreply.github.com>
2025-03-02 09:12:52 +0400

    Routing

---
+++ Respond_json.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hello, World!");
+});
+
+// Dynamic Routing
+app.get("/products", (req,res)=>{
+    const product_list = [
+        {
+        "id":1,
+        "product_name":"Shampoo"
+        },
+        {
+        "id":2,
+        "product_name":"Air Conditioner"
+        },
+        {
+        "id":3,
+        "product_name":"Sunscreen"
+        }
+    ];
+    res.json(product_list);
+});
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Server is runnin on port ${port}`);
+});
